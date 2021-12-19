8079c4f999c515b8332cd633d600a73dbce66c00
onechiporenko <oo.nechiporenko@gmail.com>
2021-12-19 16:42:33 +0200

    No global Ember

--- tests/integration/test-helper.ts
+++
@@ -1,9 +0,0 @@
-import hbs from 'htmlbars-inline-precompile';
-
-export function createCalculatorTemplate() {
-  return hbs`{{calculating-device}}`;
-}
-
-export function createInputsTemplate() {
-  return hbs`{{input-elements}}`;
-}
